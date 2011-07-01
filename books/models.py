e1c4367c90229c4142af67c668fae231278b50a6
jin <infinitus.internal@gmail.com>
2011-07-01 11:26:09 +0800

    completed chapter 5: models

--- books/models.py
+++ books/models.py
@@ -14,6 +14,11 @@ class Publisher(models.Model):
 
 
 
+    class Meta:
+        ordering = ['name']
+
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
