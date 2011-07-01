9629109321b2f6675ab9e3b62826d805868e5e87
jin <infinitus.internal@gmail.com>
2011-07-02 03:13:09 +0800

    improved form-handling by combining search_form into search

--- books/views.py
+++ books/views.py
@@ -8,10 +8,12 @@ def search_form(request):
 
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    error = False
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html', {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please submit a search term.')
-
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html', {'books': books, 'query': q})
+    return render_to_response('search_form.html', {'error': error})
