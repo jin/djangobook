081918a4d3022ff309582d04001f03d61b25bd17
jin <infinitus.internal@gmail.com>
2011-07-01 17:55:14 +0800

    database query using form submission successful

--- books/views.py
+++ books/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-
+from djangobook.books.models import Book
 
 def search_form(request):
     return render_to_response('search_form.html')
 
 
 def search(request):
-    if len(request.GET['q']):
-        message = 'You searched for: %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render_to_response('search_results.html', {'books': books, 'query': q})
     else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        return HttpResponse('Please submit a search term.')
+
