a99a7fd212e908a82f669c016ff6d76f5757ebb9
jin <infinitus.internal@gmail.com>
2011-07-02 03:19:30 +0800

    input validation

--- books/views.py
+++ books/views.py
@@ -8,12 +8,14 @@ def search_form(request):
 
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter a maximum of 20 characters.')
         else:
             books = Book.objects.filter(title__icontains=q)
             return render_to_response('search_results.html', {'books': books, 'query': q})
-    return render_to_response('search_form.html', {'error': error})
+    return render_to_response('search_form.html', {'errors': errors})
