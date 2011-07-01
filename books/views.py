46aaecb4ff02e2b131a776ec205a1aaf1e00cdd6
jin <infinitus.internal@gmail.com>
2011-07-02 03:30:18 +0800

    created contact form

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
 from djangobook.books.models import Book
 
 
@@ -19,3 +20,23 @@ def search(request):
             books = Book.objects.filter(title__icontains=q)
             return render_to_response('search_results.html', {'books': books, 'query': q})
     return render_to_response('search_form.html', {'errors': errors})
+
+
+def contact(request):
+    errors = []
+    if request.method == 'POST':
+        if not request.POST.get('subject', ''):
+            errors.append('Enter a subject.')
+        if not request.POST.get('message', ''):
+            errors.append('Enter a message.')
+        if request.POST.get('email') and '@' not in request.POST['email']:
+            errors.append('Enter a valid e-mail address.')
+        if not errors:
+            send_mail(
+                    request.POST['subject'],
+                    request.POST['message'],
+                    request.PST.get('email', 'noreply@example.com'),
+                    ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('contact/thanks/')
+    return render_to_response('contact_form.html', {'errors', errors})
