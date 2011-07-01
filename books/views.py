667180605a2670f8e12103002955ee59a56e69b0
jin <infinitus.internal@gmail.com>
2011-07-01 17:43:16 +0800

    form submission checking works

--- books/views.py
+++ books/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+
+def search_form(request):
+    return render_to_response('search_form.html')
+
+
+def search(request):
+    if len(request.GET['q']):
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from djangobook.views import * 
+from djangobook.books import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +11,6 @@ urlpatterns = patterns('',
         (r'^time/plus/(\d{1,2})/$', hours_ahead),
         (r'^admin/', include(admin.site.urls)),
         (r'^meta/$', display_meta),
+        (r'^search-form/$', views.search_form),
+        (r'^search/$', views.search),
 )
