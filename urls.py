f43a694642202be4b07365bc33b517ee8f305aba
jin <infinitus.internal@gmail.com>
2011-07-01 14:59:51 +0800

    added /admin to urlconf

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
 from djangobook.views import * 
+from django.contrib import admin
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
         (r'^hello/$', hello),
         (r'^time/$', current_datetime),
         (r'^time/plus/(\d{1,2})/$', hours_ahead),
+        (r'^admin/', include(admin.site.urls)),
 )
