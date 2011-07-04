2af819594c44042a74a6aaf9980287e808e8ce60
jin <infinitus.internal@gmail.com>
2011-07-04 23:34:10 +0800

    rewrote contact() to use forms library

--- contact/views.py
+++ contact/views.py
@@ -1,29 +1,21 @@
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from djangobook.contact.forms import ContactForm
 
 
 def contact(request):
-    errors = []
     if request.method == 'POST':
-        if not request.POST.get('subject', ''):
-            errors.append('Enter a subject.')
-        if not request.POST.get('message', ''):
-            errors.append('Enter a message.')
-        if request.POST.get('email') and '@' not in request.POST['email']:
-            errors.append('Enter a valid e-mail address.')
-        if not errors:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
             send_mail(
-                    request.POST['subject'],
-                    request.POST['message'],
-                    request.POST.get('email', 'noreply@example.com'),
+                    cd['subject'],
+                    cd['message'],
+                    cd.get('email', 'noreply@example.com'),
                     ['siteowner@example.com'],
-                    )
+                )
             return HttpResponseRedirect('/contact/thanks/')
-    return render_to_response('contact_form.html', {
-        'errors': errors,
-        'subject': request.POST.get('subject', ''),
-        'message': request.POST.get('message', ''),
-        'email': request.POST.get('email', ''),
-        })
-        
+    else:
+        form = ContactForm()
+    return render_to_response('contact_form.html', {'form': form})
