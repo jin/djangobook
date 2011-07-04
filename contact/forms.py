917cf9180151453d969da5a5bb551577c5504e5c
jin <infinitus.internal@gmail.com>
2011-07-04 23:47:17 +0800

    adding custom labels for fields

--- contact/forms.py
+++ contact/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length = 100)
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=False, label='Your e-mail address')
     message = forms.CharField(widget=forms.Textarea)
 
 
